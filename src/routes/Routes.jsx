a8144b3af848902c60ea9ab9c5c05d0a3e1640a5
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-05-09 21:50:11 +0600

    firebase and context setup

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
 import Layout from "../layout/Layout";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
-    children: [],
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 export default router;
