624cbdf576ea1941c1d4f382e2c34cb227390467
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-05-15 12:09:03 +0600

    top food with see all button

--- src/components/TopFoodSection.jsx
+++ src/components/TopFoodSection.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import TopFoodCard from "./TopFoodCard";
-import { Typography } from "@material-tailwind/react";
+import { Button, Typography } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
 const TopFoodSection = () => {
   const [foods, setFoods] = useState([]);
@@ -20,15 +21,21 @@ const TopFoodSection = () => {
           customers!
         </Typography>
         <Typography variant="paragraph">
-          we're passionate about creating exceptional food experiences. <br /> Our top
-          sellers reflect the favorites of our loyal diners, offering a
+          we're passionate about creating exceptional food experiences. <br />{" "}
+          Our top sellers reflect the favorites of our loyal diners, offering a
           delicious starting point for your culinary journey.
         </Typography>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5">
-        {foods?.map((food, idx) => (
+        {foods?.slice(0, 6).map((food, idx) => (
           <TopFoodCard food={food} key={idx} />
         ))}
+      </div>
+
+      <div className="flex justify-center items-center my-7">
+        <Link to="/allfoodpage">
+          <button className="btn btn-outline btn-md">See All</button>
+        </Link>
       </div>
     </>
   );
